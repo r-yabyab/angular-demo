8ad7f26c77f0482ce95e45a3d3340220d9b713f6
Scruffy pc <cayabyabrr@yahoo.com>
2026-07-03 17:03:42 -0700

    Create searchBar

--- src/app/search-bar/search-bar.ts
+++ src/app/search-bar/search-bar.ts
@@ -1,24 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'search-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
 
+  searchTerm = ''
   private http = inject(HttpClient)
 
   ngOnInit() {
     this.getData()
   }
 
-  getData() {
+  getData(searchTerm?: string) {
     this.http.get("https://dummyjson.com/test")
       .subscribe(data => {
         console.log(data)
       })
   }
+
+  onSearch() {
+    this.getData(this.searchTerm);
+    this.searchTerm = '';
+  }
+
 }
